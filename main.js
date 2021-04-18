c35bdfeb0eab3fdf8c3dbc79741973558c5ff594
Your Name <chornak17@gmail.com>
2021-04-18 05:33:45 +0200

    lesson03

--- main.js
+++ main.js
@@ -1,21 +1,52 @@
-//додаткова робота
-
-let num = 266219;
-
-//Розбивка числа на масив
-let splitNum = num.toString().split("");
-console.log(splitNum);
-
-//Множення кожного числа мfcbdf
-let multiplication = splitNum.reduce(function (a, b) {
-  return a * b;
-});
-console.log(multiplication);
-
-//Даний результат приводимо до 3 степені
-let power = multiplication ** 3;
-console.log(power);
-
-//Вивелення перших двух значень результатат з попередьоїзадачі
-console.log(power.toString().substr(0, 2));
+//додаткова робта 3
+let lang;
+let days;
+let days2;
 //
+//
+//if
+//
+if ((lang = "ru")) {
+  days = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
+} else if ((lаng = "en")) {
+  days = ["mn", "ts", "wd", "th", "fr", "st", "sn"];
+}
+alert(days);
+//
+//
+//switch-case
+switch (lang) {
+  case "ru":
+    days2 = ["пн", "вт", "ср", "чт", "пт", "сб", "вс"];
+    break;
+  case "en":
+    days2 = ["mn", "ts", "wd", "th", "fr", "st", "sn"];
+    break;
+}
+alert(days2);
+//
+//
+//многомерный массив
+let days3 = {
+  ru: ["пн", "вт", "ср", "чт", "пт", "сб", "вс"],
+  en: ["mn", "ts", "wd", "th", "fr", "st", "sn"],
+};
+alert(days3[lang]);
+//
+//
+//
+//
+//
+//
+//
+//
+//переменная namePerson
+let namePerson = prompt("Ваше имя");
+let result;
+result =
+  namePerson === "Артем"
+    ? "Директор"
+    : namePerson === "Максим"
+    ? "Преподаватель"
+    : "Cтудент";
+console.log(result);
