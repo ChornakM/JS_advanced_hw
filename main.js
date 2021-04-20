f1dd5b107018d4962aeacc5a91df3580fa71ce3d
Your Name <chornak17@gmail.com>
2021-04-20 09:58:38 +0200

    fix hw

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function hwFunction(start) {
-  if (typeof start !== "string") {
+  if (!isNaN(start)) {
     alert("Введите пожалуйста данные правильноon");
     return;
   }
@@ -7,6 +7,4 @@ function hwFunction(start) {
   return start.length > 30 ? start.slice(0, 30) + "..." : start;
 }
 
-console.log(hwFunction("uuuuuuthruuuuuuuuu uuuuuuuuudduuuuu"));
-
-//я це в гуглi знайшов тыльки в мене trim чомусь непрацюэ, може я його не правильно присвоїв? хоча я читав що він убирає пробіли
+console.log(hwFunction(1));
