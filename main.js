cb95e60428f8ab5b69fe7b79f8f9b34753dbc0d3
Your Name <chornak17@gmail.com>
2021-05-11 10:20:46 +0200

    lesson05

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-function hwFunction(start) {
-  if (!isNaN(start)) {
-    alert("Введите пожалуйста данные правильноon");
-    return;
+arr = ["21", "22", "55", "448", "867", "221", "472"];
+for (let i = 0; i < arr.length; i++) {
+  let currentElem = arr[i];
+  currentElem = String(currentElem);
+  if (currentElem[0] == 4 || currentElem[0] == 2) {
+    console.log(currentElem);
   }
-  start = start.trim();
-  return start.length > 30 ? start.slice(0, 30) + "..." : start;
 }
 
-console.log(hwFunction(1));
+let n = 100;
+
+nextPrime: for (let i = 2; i <= n; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j == 0) continue nextPrime;
+  }
+
+  console.log(i + " " + " Делители этого числа: 1 и " + i);
+}
