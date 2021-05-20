11215d8d1c0c2db2b98478bc44145ab69d728c1d
Your Name <chornak17@gmail.com>
2021-05-20 20:04:58 +0200

    lesson07

--- main.js
+++ main.js
@@ -1,18 +1,1 @@
-arr = ["21", "22", "55", "448", "867", "221", "472"];
-for (let i = 0; i < arr.length; i++) {
-  let currentElem = arr[i];
-  currentElem = String(currentElem);
-  if (currentElem[0] == 4 || currentElem[0] == 2) {
-    console.log(currentElem);
-  }
-}
-
-let n = 100;
-
-nextPrime: for (let i = 2; i <= n; i++) {
-  for (let j = 2; j < i; j++) {
-    if (i % j == 0) continue nextPrime;
-  }
-
-  console.log(i + " " + " Делители этого числа: 1 и " + i);
-}
+let;
