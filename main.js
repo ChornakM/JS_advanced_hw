ca4b01f5b59848560496aa5870a61b04d8c517a8
Your Name <chornak17@gmail.com>
2021-05-20 20:05:12 +0200

    lesson07

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-let;
+let week = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+let day = document.createElement("days");
+console.log(week);
+let today = prompt("This day");
+//
+//
+for (let i = 0; i < 7; i++) {
+  if (week[i] == "Saturday" || week[i] == "Sunday") {
+    document.write(`<p><i>${week[i]}</i></p>`);
+  } else if (week[i] == today) {
+    document.write(`<p><b>${week[i]}</b></p>`);
+  } else {
+    document.write(`<p>${week[i]}</p>`);
+  }
+}
